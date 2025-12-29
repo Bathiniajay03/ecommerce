41beef0d320cd7acdfb533a60c28603f765e4e8e
Ajay Bathini <ajay.bathini@themindspire.com>
2025-12-29 16:53:33 +0530

    Replace Owl Carousel with react-multi-carousel

--- src/stores/components/Mobiles.jsx
+++ src/stores/components/Mobiles.jsx
@@ -1,51 +1,50 @@
-import React, { useState, useEffect } from "react";
-import OwlCarousel from "react-owl-carousel3";
+import React from "react";
 import { mobileData } from "../data/mobiles";
 import { Link } from "react-router-dom";
 
-// Import Owl Carousel styles
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 const Mobiles = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true); // Ensures OwlCarousel loads only on client-side
-  }, []);
-
-  // Display only the first 9 mobile images
   const firstNineImages = mobileData.slice(0, 20);
- 
-  const options = {
-    loop: true,
-    margin: 11,
-    nav: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplayTimeout: 6000, // Adjust autoplay speed
-    responsive: {
-      0: { items: 1 },
-      300: { items: 8 },
-      100: { items: 6 },
-    },
-  };
+
   return (
-    <>
     <div className="scroll-container">
-      {isClient && ( // Prevents hydration issues in React
-        <OwlCarousel className="owl-theme" {...options}>
-          {firstNineImages.map((item) => (
-            <div className="imgBox" key={item.id}>
-              <Link to="/mobiles">
-                <img className="proImage" src={item.image} alt={`Mobile ${item.id}`} />
-              </Link>
-            </div>
-          ))}
-        </OwlCarousel>
-      )}
+      <Carousel
+        responsive={responsive}
+        infinite
+        autoPlay
+        autoPlaySpeed={6000}
+        arrows
+      >
+        {firstNineImages.map((item) => (
+          <div className="imgBox" key={item.id}>
+            <Link to="/mobiles">
+              <img
+                className="proImage"
+                src={item.image}
+                alt={`Mobile ${item.id}`}
+              />
+            </Link>
+          </div>
+        ))}
+      </Carousel>
     </div>
-    </>
   );
 };
 
